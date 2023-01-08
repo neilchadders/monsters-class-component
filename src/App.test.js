56324c11b74de83569a65ca0811f76059df4b110
test <test@tests-MacBook-Air.local>
2023-01-08 12:12:32 +0000

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import CardList from './components/card-list/card-list.component'
 import App from './App';
 
 test('renders learn react link', () => {
